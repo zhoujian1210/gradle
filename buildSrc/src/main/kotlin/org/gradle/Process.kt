e8252471de594a2d1760561049c38ba878c47e46
Rodrigo B. de Oliveira <rodrigo@gradle.com>
2018-01-11 16:37:32 -0200

    Convert `gradle/distributionTesting.gradle#forEachJavaProcess` to Kotlin

--- buildSrc/src/main/kotlin/org/gradle/Process.kt
+++ buildSrc/src/main/kotlin/org/gradle/Process.kt
@@ -1,9 +1,14 @@
-@file:JvmName("Process") // To make it easier to access these functions from Groovy
+// To make it easier to access these functions from Groovy
+@file:JvmName("Process")
+
 package org.gradle.process
 
+import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.internal.os.OperatingSystem
+import org.gradle.testing.LeakingProcessKillPattern
 import java.io.ByteArrayOutputStream
+
 
 fun Project.pkill(pid: String) {
     val killOutput = ByteArrayOutputStream()
@@ -29,6 +34,68 @@ Output: $killOutput
         }
     }
 }
+
+
+data class ProcessInfo(val pid: String, val process: String)
+
+
+fun Project.forEachLeakingJavaProcess(action: Action<ProcessInfo>) {
+    val output = ByteArrayOutputStream()
+    val error = ByteArrayOutputStream()
+    val (result, pidPattern) =
+        if (isWindows) {
+            exec {
+                commandLine("wmic", "process", "get", "processid,commandline")
+                standardOutput = output
+                errorOutput = error
+                isIgnoreExitValue = true
+            } to "([0-9]+)\\s*$".toRegex()
+        } else {
+            exec {
+                commandLine("ps", "x")
+                standardOutput = output
+                errorOutput = output
+                isIgnoreExitValue = true
+            } to "([0-9]+)".toRegex()
+        }
+
+    if (result.exitValue != 0) {
+        val errorLog = file("${rootProject.buildDir}/errorLogs/process-list-${System.currentTimeMillis()}.log")
+        mkdir(errorLog.parent)
+        errorLog.writeText("[Output]\n$output\n[Error Output]\n$error")
+        logger.quiet("Error obtaining process list, output log created at $errorLog")
+        result.assertNormalExitValue()
+    }
+
+    val processPattern = generateLeakingProcessKillPattern()
+    forEachLineIn(output.toString()) { line ->
+        val processMatcher = processPattern.find(line)
+        if (processMatcher != null) {
+            val pidMatcher = pidPattern.find(line)
+            if (pidMatcher != null) {
+                val pid = pidMatcher.groupValues[1]
+                val process = processMatcher.groupValues[1]
+                if (!isMe(process)) {
+                    action.execute(ProcessInfo(pid, process))
+                }
+            }
+        }
+    }
+}
+
+
+fun Project.generateLeakingProcessKillPattern() =
+    LeakingProcessKillPattern.generate(rootProject.projectDir.absolutePath).toRegex()
+
+
+inline
+fun forEachLineIn(s: String, action: (String) -> Unit) =
+    s.lineSequence().forEach(action)
+
+
+fun Project.isMe(process: String) =
+    process.contains(gradle.gradleHomeDir!!.path)
+
 
 val isWindows get() = OperatingSystem.current().isWindows
 
